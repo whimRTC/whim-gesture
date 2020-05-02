08be3eb2f61474dc82a8833ae03f4c5b2a23c3f5
Odagawa <xtctl.1234@gmail.com>
2020-05-03 01:46:42 +0900

    new firebases

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -7,14 +7,40 @@ import 'firebase/firestore'
 Vue.use(firestorePlugin)
 
 // .env に設定した値を取得してる
-const config = {
-  apiKey: "AIzaSyBvmI6z0CywFtR47HNZoR7_STqRzuPJZ-Q",
-  authDomain: "ica-pj.firebaseapp.com",
-  databaseURL: "https://ica-pj.firebaseio.com",
-  projectId: "ica-pj",
-  storageBucket: "ica-pj.appspot.com",
-  messagingSenderId: "513008473344",
-  appId: "1:513008473344:web:7509a92a412e839c688fe5"
+let config
+if (process.env.whimEnv == 'development'){
+  config = {
+    apiKey: "AIzaSyAicixU5yd1NofCbDyk5iI18m2weaPhLjA",
+    authDomain: "whim-dev.firebaseapp.com",
+    databaseURL: "https://whim-dev.firebaseio.com",
+    projectId: "whim-dev",
+    storageBucket: "whim-dev.appspot.com",
+    messagingSenderId: "536342426469",
+    appId: "1:536342426469:web:a0b4e854d1543ff534e78c"
+  };
+} else if (process.env.whimEnv == 'staging'){
+  config = {
+    apiKey: "AIzaSyA7ncGgKhQYZhk1ffs2qlyfIj3f5yKAR4s",
+    authDomain: "whim-stg.firebaseapp.com",
+    databaseURL: "https://whim-stg.firebaseio.com",
+    projectId: "whim-stg",
+    storageBucket: "whim-stg.appspot.com",
+    messagingSenderId: "180734884575",
+    appId: "1:180734884575:web:7c3c030762d45708acc76a"
+    };
+} else if (process.env.whimEnv == 'production'){
+  config = {
+    apiKey: "AIzaSyDHOPm12ei_7vJa9EcsnrbhwvueqeJtThY",
+    authDomain: "whim-prd.firebaseapp.com",
+    databaseURL: "https://whim-prd.firebaseio.com",
+    projectId: "whim-prd",
+    storageBucket: "whim-prd.appspot.com",
+    messagingSenderId: "4728040924",
+    appId: "1:4728040924:web:0fca1ff77845853d12b319",
+    measurementId: "G-2C52S3GFCC"
+  };
+} else {
+  throw new Error("環境変数が無効または設定されていません。whimEnvに環境変数を設定してください。");
 }
 
 if (!firebase.apps.length) {
